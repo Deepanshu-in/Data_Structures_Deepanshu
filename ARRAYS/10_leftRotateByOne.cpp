9f5a5db5c3626f52e8091aca154e3d7f5e08d29e
Deepanshu <dipanshugupta921@gmail.com>
2022-12-28 19:27:52 +0530

    new commit

--- ARRAYS/10_leftRotateByOne.cpp
+++ ARRAYS/10_leftRotateByOne.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-void leftRotate(int arr[], int n)
+void lefttRotate(int arr[], int n)
 {
     int temp = arr[0];
     for (int i = 1; i < n; i++)
@@ -12,7 +12,7 @@ void leftRotate(int arr[], int n)
 int main()
 {
     int arr[5] = {1, 2, 3, 4, 5};
-    leftRotate(arr, 5);
+    lefttRotate(arr, 5);
 
     for (int i = 0; i < 5; i++)
     {
