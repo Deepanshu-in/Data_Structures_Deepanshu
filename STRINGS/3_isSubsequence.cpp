5e2aba806eba83ac2a4f23f48e560a4fd9b601f7
Deepanshu <dipanshugupta921@gmail.com>
2023-01-05 16:09:58 +0530

    New Commit

--- STRINGS/3_isSubsequence.cpp
+++ STRINGS/3_isSubsequence.cpp
@@ -1,15 +1,20 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+using namespace std;
+bool isSub(string s1, string s2, int n, int m)
+{
+    int j = 0;
+    for (int i = 0; i < n && j < m; i++)
+    {
+        if (s1[i] == s2[j])
+            j++;
+    }
+    return (j == m);
+}
 int main()
 {
-    if (__cplusplus == 201703L)
-        std::cout << "C++17\n";
-    else if (__cplusplus == 201402L)
-        std::cout << "C++14\n";
-    else if (__cplusplus == 201103L)
-        std::cout << "C++11\n";
-    else if (__cplusplus == 199711L)
-        std::cout << "C++98\n";
-    else
-        std::cout << "pre-standard C++\n";
+    string s1 = "ABCDE";
+    string s2 = "ACDE";
+    int j = isSub(s1, s2, 5, 4);
+    cout << j;
+    return 0;
 }
