6b7c320f0c6674cf7baf8d09ade95635e3c616e8
Deepanshu <dipanshugupta921@gmail.com>
2023-01-08 00:19:56 +0530

    New Commit

---
+++ ARRAYS/16_Longest_Even_odd_subarray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestEvenOdd(int arr[], int n)
+{
+    int res = 1;
+    int count = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] % 2 == 0 && arr[i - 1] % 2 == 1 || arr[i] % 2 == 1 && arr[i - 1] % 2 == 0)
+        {
+            count++;
+            res = max(res, count);
+        }
+        else
+            count = 1;
+    }
+    return res;
+}
+
+int main()
+{
+    int arr[6] = {5, 10, 20, 6, 3, 8};
+    int j = longestEvenOdd(arr, 6);
+    cout << j;
+
+    return 0;
+}
---
+++ ARRAYS/17_Majority.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int majority(int arr[], int n)
+{
+    int res = 0;
+    int count = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[res] == arr[i])
+            count++;
+        else
+            count--;
+        if (count == 0)
+        {
+            res = i;
+            count = 1;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+
+    return 0;
+}
