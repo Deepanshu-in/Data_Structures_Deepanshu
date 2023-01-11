63557c56b563d229cb25b30e96796096c75b6f76
Deepanshu <dipanshugupta921@gmail.com>
2023-01-11 22:40:25 +0530

    Adding Linkedlist folder

---
+++ LINKEDLIST/1_implementation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+
+    cout << head->data;
+    cout << head->next->data;
+    cout << head->next->next->data;
+    return 0;
+}
