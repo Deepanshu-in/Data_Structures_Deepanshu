61a8fd40c6b1b84a9fa7d6a881ea9ba633fad791
Deepanshu <dipanshugupta921@gmail.com>
2023-01-15 01:58:56 +0530

    New commit

---
+++ LINKEDLIST/SINGLY CIRCULAR/3_insert_at_begin.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+void printLL(Node *head)
+{
+    if (head == NULL)
+        return;
+    Node *p = head;
+    do
+    {
+        cout << p->data << " ";
+        p = p->next;
+    } while (p != head);
+    cout << endl;
+}
+
+Node *insertAtBegin(Node *head, int x)
+{
+    Node *temp = new Node(x);
+    if (head == NULL)
+        return temp;
+    else
+    {
+        Node *curr = head;
+        while (curr->next != head)
+            curr = curr->next;
+        curr->next = temp;
+        temp->next = head;
+        return temp;
+    }
+}
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(5);
+    head->next->next = new Node(20);
+    head->next->next->next = new Node(15);
+    head->next->next->next->next = head;
+    printLL(head);
+
+    head = insertAtBegin(head, 30);
+    printLL(head);
+    return 0;
+}
---
+++ LINKEDLIST/SINGLY CIRCULAR/7_delete_head.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+Node *delHead(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL)
+    {
+        delete head;
+        return NULL;
+    }
+    head->data = head->next->data;
+    Node *temp = head->next;
+    head->next = head->next->next;
+    delete temp;
+    return head;
+}
+void printlist(Node *head)
+{
+    if (head == NULL)
+        return;
+    Node *p = head;
+    do
+    {
+        cout << p->data << " ";
+        p = p->next;
+    } while (p != head);
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = head;
+    head = delHead(head);
+    printlist(head);
+    return 0;
+}
