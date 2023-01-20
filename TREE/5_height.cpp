70e74809e26b341aa8b95a53e759e94aca48d4e1
Deepanshu <dipanshugupta921@gmail.com>
2023-01-20 19:02:20 +0530

    new commit

--- TREE/5_height.cpp
+++ TREE/5_height.cpp
@@ -15,11 +15,22 @@ struct Node
     }
 };
 
+int height(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    else
+        return max(height(root->left), height(root->right)) + 1;
+}
+
 int main()
 {
     Node *root = new Node(20);
     root->left = new Node(30);
     root->right = new Node(40);
     root->left->left = new Node(5);
+    root->right->left = new Node(60);
+    int c = height(root);
+    cout << c;
     return 0;
 }
--- TREE/6_kth_node.cpp
+++ TREE/6_kth_node.cpp
@@ -14,6 +14,18 @@ struct Node
         right = NULL;
     }
 };
+void knode(Node *root, int k)
+{
+    if (root == NULL)
+        return;
+    if (k == 0)
+        cout << root->data << " ";
+    else
+    {
+        knode(root->left, k - 1);
+        knode(root->right, k - 1);
+    }
+}
 
 int main()
 {
@@ -21,5 +33,7 @@ int main()
     root->left = new Node(30);
     root->right = new Node(40);
     root->left->left = new Node(5);
+    root->right->left = new Node(60);
+    knode(root, 2);
     return 0;
 }
